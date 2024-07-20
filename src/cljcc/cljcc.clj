76041390b66ae1a7e903bfd40b3b176dc895b385
Shagun Agrawal <agrawalshagun07@gmail.com>
2024-07-21 00:11:14 +0530

    add parser for ch1

--- src/cljcc/cljcc.clj
+++ src/cljcc/cljcc.clj
@@ -1,5 +1,27 @@
 (ns cljcc.cljcc
+  (:require
+   [instaparse.core :as insta]
+   [clojure.java.io :as io])
   (:gen-class))
+
+(def ex-prg "int main(void) {return 2;}")
+
+(def whitespace
+  (insta/parser
+   "whitespace = #'\\s+'"))
+
+(def c-parser
+  (insta/parser
+   "<program> = function+
+    function = #'int\\b' identifier <'('> #'void\\b' <')'> <'{'> statement <'}'>
+    statement = #'return\\b' exp <';'>
+    exp = constant
+    identifier = #'[a-zA-Z_]\\w*\\b'
+    constant = #'[0-9]+\\b'
+    keyword = #'int\\b' | #'return\\b' | #'void\\b'"
+   :auto-whitespace whitespace))
+
+(println (c-parser ex-prg))
 
 (defn greet
   "Callable entry point to the application."
@@ -9,4 +31,5 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (let [input-file-path (first args)])
   (greet {:name (first args)}))
